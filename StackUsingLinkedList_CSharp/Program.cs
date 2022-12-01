9f37b6ddf76c21848fd8c49b9574bd891a0e09f4
Dianitsya <dianitasyanandamasta@gmail.com>
2022-12-01 15:57:10 +0700

    add method di  class Stack

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -15,6 +15,52 @@ namespace StackUsingLinkedList_CSharp
     class Stacks
     {
 
-       
+        Node top;
+
+        public Stacks()
+        {
+            top = null;
+        }
+
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
+
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, top);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + " pushed.");
+        }
+
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node in sequence
+        }
+
+        public void display()
+        {
+            Node tmp;
+            if (empty()) //if stack is empty
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                //Traverse the list from begginning till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
+        
     }
 }
